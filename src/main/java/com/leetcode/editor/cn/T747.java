d422581f5efbe52eee6a39e6ee46ef236845af2a
nullcxy <1287530995@qq.com>
2020-04-03 13:26:55 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/editor/cn/T747.java
@@ -0,0 +1,61 @@
+package com.leetcode.editor.cn;
+
+//在一个给定的数组nums中，总是存在一个最大元素 。 
+//
+// 查找数组中的最大元素是否至少是数组中每个其他数字的两倍。 
+//
+// 如果是，则返回最大元素的索引，否则返回-1。 
+//
+// 示例 1: 
+//
+// 输入: nums = [3, 6, 1, 0]
+//输出: 1
+//解释: 6是最大的整数, 对于数组中的其他整数,
+//6大于数组中其他元素的两倍。6的索引是1, 所以我们返回1.
+// 
+//
+// 
+//
+// 示例 2: 
+//
+// 输入: nums = [1, 2, 3, 4]
+//输出: -1
+//解释: 4没有超过3的两倍大, 所以我们返回 -1.
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// nums 的长度范围在[1, 50]. 
+// 每个 nums[i] 的整数范围在 [0, 100]. 
+// 
+// Related Topics 数组
+
+
+import java.util.Arrays;
+
+public class T747{
+    public static void main(String[] args) {
+        int[] nums = {1,0};
+        int result = dominantIndex(nums);
+    }
+
+    public static int dominantIndex(int[] nums) {
+        int[] temp = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums);
+        int max = nums[nums.length-1];
+        int next = nums[nums.length-2];
+        if(max>=next*2){
+            for (int i = 0; i < temp.length; i++) {
+                if(temp[i]==max){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

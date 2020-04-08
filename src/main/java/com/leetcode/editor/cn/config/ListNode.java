4d3525d3e5fe8f9b57debd09f8a9282b7c68f46b
nullcxy <1287530995@qq.com>
2020-04-08 13:51:46 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/editor/cn/config/ListNode.java
@@ -0,0 +1,16 @@
+package com.leetcode.editor.cn.config;
+
+/**
+ * @Author: Evan
+ * @CreateTime: 2020-04-08
+ * @Description:
+ */
+public class ListNode {
+    public static int val;
+
+    public static ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}

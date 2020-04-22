1752e38b4afe63daffb96565d9aec693a3ee3320
nullcxy <1287530995@qq.com>
2020-04-22 18:00:26 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/editor/cn/T242.java
@@ -0,0 +1,45 @@
+package com.leetcode.editor.cn;
+
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。 
+//
+// 示例 1: 
+//
+// 输入: s = "anagram", t = "nagaram"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: s = "rat", t = "car"
+//输出: false 
+//
+// 说明: 
+//你可以假设字符串只包含小写字母。 
+//
+// 进阶: 
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 排序 哈希表
+
+
+public class T242{
+    public static void main(String[] args) {
+        isAnagram("a","ab");
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()<t.length()){
+            String tem = s;
+            s = t;
+            t = tem;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            String temp = s.substring(i,i+1);
+            if(t.contains(temp)){
+                t = t.replaceFirst(temp,"");
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}

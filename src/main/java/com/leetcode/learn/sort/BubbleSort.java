4c951776b8178e028ee6dd5aa1a277f524fa3a2c
nullcxy <1287530995@qq.com>
2020-07-23 11:43:46 +0800

    冒泡排序

---
+++ src/main/java/com/leetcode/learn/sort/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.leetcode.learn.sort;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Evan
+ * @CreateTime: 2020-07-23
+ * @Description: 冒泡排序
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = {9,2,11};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i+1; j < array.length; j++) {
+//                if (array[i] > array[j]) {
+//                    int temp = array[j];
+//                    array[j] = array[i];
+//                    array[i] = temp;
+//                }
+//            }
+//        }
+        System.out.println(Arrays.toString(array));
+    }
+}

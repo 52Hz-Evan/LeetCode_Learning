8ac720d00425c5044554c411d582ca1f3b6b76a8
nullcxy <1287530995@qq.com>
2020-07-23 11:58:35 +0800

    冒泡排序

--- src/main/java/com/leetcode/learn/sort/BubbleSort.java
+++ src/main/java/com/leetcode/learn/sort/BubbleSort.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 /**
  * @Author: Evan
  * @CreateTime: 2020-07-23
- * @Description: 冒泡排序
+ * @Description: 冒泡排序：O(n^2)
  */
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] array = {9,2,11};
+        int[] array = {9,2,11,7,12,5,54,57,84,12,43,23};
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j+1]) {

9c39616368a6a2062bf5514793be40b0e00f0bfc
nullcxy <1287530995@qq.com>
2020-07-29 13:16:09 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/editor/cn/T912.java
@@ -0,0 +1,71 @@
+package com.leetcode.editor.cn;
+
+//给你一个整数数组 nums，请你将该数组升序排列。 
+//
+// 
+//
+// 
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [5,2,3,1]
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [5,1,1,2,0,0]
+//输出：[0,0,1,1,2,5]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 50000 
+// -50000 <= nums[i] <= 50000 
+// 
+//
+
+
+public class T912 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    //快速排序
+    private static void quickSort(int s[], int l, int r) {
+        if (l < r) {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j) {
+                while (i < j && s[j] >= x) {
+                    // 从右向左找第一个小于x的数
+                    j--;
+                }
+
+                if (i < j) {
+                    s[i++] = s[j];
+                }
+
+                while (i < j && s[i] < x) { // 从左向右找第一个大于等于x的数
+                    i++;
+                }
+                if (i < j) {
+                    s[j--] = s[i];
+                }
+            }
+            s[i] = x;
+            quickSort(s, l, i - 1); // 递归调用
+            quickSort(s, i + 1, r);
+        }
+    }
+
+}

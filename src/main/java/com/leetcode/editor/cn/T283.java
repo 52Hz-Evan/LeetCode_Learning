683a25dfa54dd684795dcc077e61df265de3e843
52Hz-Evan <1287530995@qq.com>
2021-01-18 13:25:39 +0800

    LeetCode

---
+++ src/main/java/com/leetcode/editor/cn/T283.java
@@ -0,0 +1,56 @@
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+//
+// 示例: 
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0] 
+//
+// 说明: 
+//
+// 
+// 必须在原数组上操作，不能拷贝额外的数组。 
+// 尽量减少操作次数。 
+// 
+// Related Topics 数组 双指针 
+// 👍 916 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class T283 {
+    public static void main(String[] args) {
+        moveZeroes(new int[]{0, 1, 0, 3, 12});
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                if (i != index) {
+                    nums[i] = 0;
+                }
+                index++;
+            }
+        }
+//        int j = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] != 0) {
+//                nums[j] = nums[i];
+//                if (i != j) {
+//                    nums[i] = 0;
+//                }
+//                j++;
+//            }
+//        }
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

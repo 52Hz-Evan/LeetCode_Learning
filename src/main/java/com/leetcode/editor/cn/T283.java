3db444403acd6589c3ee9a97769dbe6791c33562
52Hz-Evan <1287530995@qq.com>
2021-01-18 13:31:19 +0800

    LeetCode

--- src/main/java/com/leetcode/editor/cn/T283.java
+++ src/main/java/com/leetcode/editor/cn/T283.java
@@ -22,16 +22,6 @@ public class T283 {
     }
 
     public static void moveZeroes(int[] nums) {
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                if (i != index) {
-                    nums[i] = 0;
-                }
-                index++;
-            }
-        }
 //        int j = 0;
 //        for (int i = 0; i < nums.length; i++) {
 //            if (nums[i] != 0) {
